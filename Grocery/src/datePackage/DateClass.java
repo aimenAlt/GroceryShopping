0288fcc925f11759a6563ee101e837d2d4d132ad
aimenAlt <work@Aimens-MacBook-Pro-2.local>
2021-10-25 13:12:26 -0700

    Complete Date Class

--- Grocery/src/datePackage/DateClass.java
+++ Grocery/src/datePackage/DateClass.java
@@ -1,5 +1,19 @@
 package datePackage;
 
 public class DateClass {
-
+	
+	private int month = 0;
+	private int day = 0;
+	private int year = 0;
+	
+	DateClass(int month, int day, int year) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+	}
+	
+	public String toString() {
+		String returningString = this.month + "/" + this.day + "/" + this.year;
+		return returningString;
+	}
 }
